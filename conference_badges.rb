c5d1c8d8c36bdc1ceda332e3ad2de8c2659b5991
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-25 02:04:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array_of_names)
-  array_of_names.each do |name|
-    
+def batch_badge_creator(attendees)
+  batch_badge_printed = []
+  attendees.each do |attendee|
+    batch_badge_printed << "Hello, my name is #{attendee}."
 end 
